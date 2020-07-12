2d305e656a19dc611f3711c6d9573926b5ed0973
Aryan Likhite <aryanlikhite@gmail.com>
2020-07-12 20:38:15 +0530

    Added the delete meet and settings column

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ const express = require('express')
 const path = require('path');
 const Gmeet = require('./lib/Gmeet');
 const bodyParser = require('body-parser')
+const shortid = require('shortid');
 const app = express()
 
 const gmeet = new Gmeet()
 
+const HEADLESS = false //Change this to true if you dont want the chrome UI to show up
+const VERBOSE = false // Change this to true to get extra messages in the console
+const CHECKTIME = 10000 // This variable determines how often will the code check for meeting. Value in miliseconds
+
 let meetings = {}
+let config = {
+	email: "Enter you email here",
+	password: "Enter the password for the email",
+}
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,20 +26,36 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-	res.render('index', {meetings})
+	res.render('index', {meetings, config})
 });
 
 app.post('/submitMeet', (req, res) => {
-	meetings[req.body.meetName] = {}
-	meetings[req.body.meetName].time = Date.parse(req.body.meetTime)
-	meetings[req.body.meetName].url = req.body.meetUrl
+	let uid = shortid.generate()
+	meetings[uid] = {}
+	meetings[uid].name = req.body.meetName
+	meetings[uid].time = Date.parse(req.body.meetTime)
+	meetings[uid].url = req.body.meetUrl
 	res.redirect("/")
+});
+
+app.post('/delete', (req, res) => {
+	let id = req.body.delID
+	delete meetings[id]
+	res.redirect('/')
+});
+
+app.post('/settings', (req, res) => {
+	let email = req.body.email
+	let password = req.body.password
+	config['email'] = email
+	config['password'] = password
+	res.redirect('/')
 });
 
 const checkForMeeting = () => {
 	for(meeting in meetings) {
 		if(Date.now() > meetings[meeting].time) {
-			gmeet.join(meetings[meeting].url)
+			gmeet.join(meetings[meeting].url, config.email, config.password, {headless: HEADLESS, verbose: VERBOSE})
 			delete meetings[meeting]
 		}
 	}
@@ -40,6 +65,6 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
 	setInterval(() => {
 		checkForMeeting()
-	}, 10000)
+	}, CHECKTIME)
 	console.log('App listening on port 3000!');
 });
--- public/js/index.js
+++
@@ -1,6 +0,0 @@
-// let meetSubmit = document.querySelector("#meetSubmit")
-
-// meetSubmit.onclick = () => {
-// 	let meetName = document.querySelector("#meetName").value
-// 	alert(meetName)
-// }
